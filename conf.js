c6af0e588b4e208b9543f96de4a1dabe68fe1134
tuanpham86 <tuan.phamvan86@gmail.com>
2016-05-15 22:13:26 +0700

    pending

--- conf.js
+++ conf.js
@@ -1,6 +1,26 @@
 // conf.js
 exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js'],
+    capabilities: {
+        browserName: 'firefox'
+    },
+    jasmineNodeOpts: {
+        showColors: true, // Use colors in the command line report.
+    }
 }
+
+/*
+// conf.js
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js'],
+    multiCapabilities: [{
+        browserName: 'firefox'
+    }, {
+        browserName: 'chrome'
+    }]
+}
+*/
--- spec.js
+++ spec.js
@@ -1,13 +1,62 @@
 // spec.js
+/*
 describe('Protractor Demo App', function() {
-  it('should add one and two', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+
+  beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+  });
 
-    element(by.id('gobutton')).click();
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
 
-    expect(element(by.binding('latest')).getText()).
-        toEqual('3'); // This is wrong!
+  it('should add one and two', function() {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', function() {
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+      
+    expect(latestResult.getText()).toEqual('10');
+  });
+});
+*/
+
+// spec.js
+describe('Protractor Demo App', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
+
+  beforeEach(function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+  });
+
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
+
+    expect(history.last().getText()).toContain('1 + 2');
+    expect(history.first().getText()).toContain('3 + 4');
   });
 });
